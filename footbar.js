3967cf769d7bc068ff8d2566dc91065470d72bd8
ki-j <165767504+ki-j@users.noreply.github.com>
2025-10-12 21:35:41 +0100

    Add files via upload

---
+++ footbar.js
@@ -0,0 +1,10 @@
+let footbarHTML = `
+    <nav>
+            <ul>
+              <li><i class="fa fa-envelope fa" style="color:#fcc367"></i>&emsp;ksj557@student.bham.ac.uk</li>
+              <li><i class="fa fa-linkedin fa-lg" style="color:#fcc367"></i>&emsp;Kiran Jhass</li>
+              <li><i style="color:#fcc367"><b>ORCiD</b></i>&emsp;0009-0007-5240-6861</li>
+            </ul>
+          </nav>
+`;
+document.write(footbarHTML);
---
+++ sidebar.js
@@ -0,0 +1,18 @@
+let sidebarHTML = `
+    <div class="sidenav">
+        <img src="C:/Users/ksj557/OneDrive - University of Birmingham/Website/sex1.png" alt="Me during an observing night at the Observatorio de Roque de los Muchachos in La Palma." style="margin-left: 10%; width: 75%; justify-content: center;"><br>
+        <br>
+        <a href="index.html">About</a>
+        <a href="projects.html">Projects</a>
+        <a href="employment.html">Employment</a>
+        <!-- <a href="cv.html">CV</a> -->
+        <a href="materials.html">Materials</a>
+        <br>
+        <a style="font-size: small; color: #fff8e7;"><b>Contact Me</b></a>
+        <li><a href="mailto:ksj557@student.bham.ac.uk"><i class="fa fa-envelope fa" style="color:#fcc367"></i>&emsp;<b>ksj557.student.bham.ac.uk</b></a></li>
+        <li><a href="https://www.linkedin.com/in/kiran-jhass-8267b01ba/"><i class="fa fa-linkedin fa-lg" style="color:#fcc367"></i>&emsp;<b>Kiran Jhass</b></a></li>
+        <li><a href="https://orcid.org/0009-0007-5240-6861"><i style="color:#fcc367"><b>ORCiD</b></i>&emsp;<b>0009-0007-5240-6861</b></a></li>
+        <li><a href="cvs/CV_academic.pdf" target="_blank"><i class="fa fa-file-pdf-o fa-lg" style="color:#fcc367"></i>&emsp;<b>CV</b></a></li>
+    </div>
+`;
+document.write(sidebarHTML);
