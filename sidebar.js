f06b50717b77286288ce46e972c366c7af65d1c3
ki-j <KSJ557@student.bham.ac.uk>
2025-12-16 23:04:36 +0000

    regular backup

--- sidebar.js
+++ sidebar.js
@@ -6,10 +6,10 @@ let sidebarHTML = `
         <a href="https://ki-j.github.io/projects.html">Projects</a>
         <a href="https://ki-j.github.io/employment.html">Employment</a>
         <!-- <a href="cv.html">CV</a> -->
-        <a href="https://ki-j.github.io/materials.html">Materials</a>
+        <!-- <a href="https://ki-j.github.io/materials.html">Materials</a> -->
         <br>
         <a style="font-size: small; color: #fff8e7;"><b>Contact Me</b></a>
-        <li><a href="mailto:ksj557@student.bham.ac.uk"><i class="fa fa-envelope fa" style="color:#fcc367"></i>&emsp;<b>ksj557.student.bham.ac.uk</b></a></li>
+        <li><a href="mailto:ksj557@student.bham.ac.uk"><i class="fa fa-envelope fa" style="color:#fcc367"></i>&emsp;<b>ksj557@student.bham.ac.uk</b></a></li>
         <li><a href="https://www.linkedin.com/in/kiran-jhass-8267b01ba/"><i class="fa fa-linkedin fa-lg" style="color:#fcc367"></i>&emsp;<b>Kiran Jhass</b></a></li>
         <li><a href="https://orcid.org/0009-0007-5240-6861"><i style="color:#fcc367"><b>ORCiD</b></i>&emsp;<b>0009-0007-5240-6861</b></a></li>
         <li><a href="cvs/CV_academic.pdf" target="_blank"><i class="fa fa-file-pdf-o fa-lg" style="color:#fcc367"></i>&emsp;<b>CV</b></a></li>
