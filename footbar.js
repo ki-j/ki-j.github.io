6fc4226b10728410d9006ba824c1e46e80683684
ki-j <KSJ557@student.bham.ac.uk>
2026-07-15 15:05:21 +0100

    for real this time, fixed sidebar

--- footbar.js
+++ footbar.js
@@ -1,10 +1,12 @@
 let footbarHTML = `
     <nav>
-            <ul>
-              <li><i class="fa fa-envelope fa" style="color:#fcc367"></i>&emsp;ksj557@student.bham.ac.uk</li>
-              <li><i class="fa fa-linkedin fa-lg" style="color:#fcc367"></i>&emsp;Kiran Jhass</li>
-              <li><i style="color:#fcc367"><b>ORCiD</b></i>&emsp;0009-0007-5240-6861</li>
-            </ul>
-          </nav>
+        <ul>
+          <li><a href="mailto:ksj557@student.bham.ac.uk"><i class="fa fa-envelope fa" style="color:#fcc367"></i>&emsp;<b>ksj557@student.bham.ac.uk</b></a></li>
+          <li><a href="https://www.linkedin.com/in/kiran-jhass-8267b01ba/"><i class="fa-brands fa-linkedin-in" style="color:#fcc367"></i>&emsp;<b>Kiran Jhass</b></a></li>
+          <li><a href="https://orcid.org/0009-0007-5240-6861"><i style="color:#fcc367"><b>ORCiD</b></i>&emsp;<b>0009-0007-5240-6861</b></a></li>
+          <li><a href="https://scholar.google.com/citations?user=Hm6CvaoAAAAJ&hl=en&oi=ao"><i class="fa-brands fa-google-scholar" style="color:#fcc367"></i>&emsp;<b>Google Scholar</b></a></li>
+          <li><a href="https://scixplorer.org/search?d=astrophysics&p=1&q=author%3A%22kiran+jhass%22&sort=score+desc&sort=date+desc"><i class="fa-duotone fa-solid fa-magnifying-glass" style="color:#fcc367"></i>&emsp;<b>SciX</b></a></li>
+        </ul>
+    </nav>
 `;
 document.write(footbarHTML);
--- sidebar.js
+++ sidebar.js
@@ -14,8 +14,10 @@ let sidebarHTML = `
         <li><a href="mailto:ksj557@student.bham.ac.uk"><i class="fa fa-envelope fa" style="color:#fcc367"></i>&emsp;<b>ksj557@student.bham.ac.uk</b></a></li>
         <li><a href="https://www.linkedin.com/in/kiran-jhass-8267b01ba/"><i class="fa-brands fa-linkedin-in" style="color:#fcc367"></i>&emsp;<b>Kiran Jhass</b></a></li>
         <li><a href="https://orcid.org/0009-0007-5240-6861"><i style="color:#fcc367"><b>ORCiD</b></i>&emsp;<b>0009-0007-5240-6861</b></a></li>
-        <li><a href="https://scholar.google.com/citations?user=Hm6CvaoAAAAJ&hl=en&oi=ao"><i class="fa-brands fa-google-scholar" style="color:#fcc367"></i>&emsp;<b>Kiran Jhass</b></a></li>
-        <li><a href="cvs/CV_kiranjhass.pdf" target="_blank"><i class="fa-solid fa-file-pdf" style="color:#fcc367"></i>&emsp;<b>CV</b></a></li>
+        <li><a href="https://scholar.google.com/citations?user=Hm6CvaoAAAAJ&hl=en&oi=ao"><i class="fa-brands fa-google-scholar" style="color:#fcc367"></i>&emsp;<b>Google Scholar</b></a></li>
+        <li><a href="https://scixplorer.org/search?d=astrophysics&p=1&q=author%3A%22kiran+jhass%22&sort=score+desc&sort=date+desc"><i class="fa-duotone fa-solid fa-magnifying-glass" style="color:#fcc367"></i>&emsp;<b>SciX</b></a></li>
+        <!-- <li><a href="cvs/CV_kiranjhass.pdf" target="_blank"><i class="fa-solid fa-file-pdf" style="color:#fcc367"></i>&emsp;<b>CV</b></a></li> -->
+        <br>
     </div>
 `;
 
