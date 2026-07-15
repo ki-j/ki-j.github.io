424cb1a4e8c86018ef5ef919d996bc98ab19e06b
ki-j <KSJ557@student.bham.ac.uk>
2026-07-15 14:33:42 +0100

    for real this time, fixed sidebar

--- sidebar.js
+++ sidebar.js
@@ -12,10 +12,10 @@ let sidebarHTML = `
         <br>
         <a style="font-size: small; color: #fff8e7;"><b>Contact Me</b></a>
         <li><a href="mailto:ksj557@student.bham.ac.uk"><i class="fa fa-envelope fa" style="color:#fcc367"></i>&emsp;<b>ksj557@student.bham.ac.uk</b></a></li>
-        <li><a href="https://www.linkedin.com/in/kiran-jhass-8267b01ba/"><i class="fa fa-linkedin fa-lg" style="color:#fcc367"></i>&emsp;<b>Kiran Jhass</b></a></li>
+        <li><a href="https://www.linkedin.com/in/kiran-jhass-8267b01ba/"><i class="fa-brands fa-linkedin-in" style="color:#fcc367"></i>&emsp;<b>Kiran Jhass</b></a></li>
         <li><a href="https://orcid.org/0009-0007-5240-6861"><i style="color:#fcc367"><b>ORCiD</b></i>&emsp;<b>0009-0007-5240-6861</b></a></li>
-        <li><a href="https://scholar.google.com/citations?user=Hm6CvaoAAAAJ&hl=en&oi=ao"><i class="fa-brands fa-google-scholar" style="color:#fcc367;"></i>&emsp;<b>Kiran Jhass</b></a></li>
-        <li><a href="cvs/CV_kiranjhass.pdf" target="_blank"><i class="fa fa-file-pdf-o fa-lg" style="color:#fcc367"></i>&emsp;<b>CV</b></a></li>
+        <li><a href="https://scholar.google.com/citations?user=Hm6CvaoAAAAJ&hl=en&oi=ao"><i class="fa-brands fa-google-scholar" style="color:#fcc367"></i>&emsp;<b>Kiran Jhass</b></a></li>
+        <li><a href="cvs/CV_kiranjhass.pdf" target="_blank"><i class="fa-solid fa-file-pdf" style="color:#fcc367"></i>&emsp;<b>CV</b></a></li>
     </div>
 `;
 
